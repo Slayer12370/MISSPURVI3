cd3394ec7506683e6ac5dc7eb0bbdce9677b46f2
Slayer12370 <manasmishra6000@gmail.com>
2024-08-17 12:46:09 +0530

    Update config.py
    Signed-off-by: Slayer12370 <manasmishra6000@gmail.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/luffyxnetwork")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Deadly_Community")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/1e56c68afcb77704e41a6.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/a6caa0b016e33168ee0e4.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
